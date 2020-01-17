c52e04ba927f69c7ea30833552333d6a7c230271
Leewel Karani <leewelkarani@gmail.com>
2020-01-17 22:34:40 +0300

    Add OAuth 2.0 Login

--- src/App.js
+++ src/App.js
@@ -1,40 +1,138 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "./Home";
+import {
+  Security,
+  SecureRoute,
+  ImplicitCallback,
+  withAuth
+} from "@okta/okta-react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CoffeeShopsList from "./CoffeeShopsList";
 import CoffeeShopEdit from "./CoffeeShopEdit";
 import Api from "./Api";
 import NavBar from "./Navbar";
 
-const api = new Api();
+const AuthWrapper = withAuth(
+  class WrappedRoutes extends Component {
+    constructor(props) {
+      super(props);
 
-class App extends Component {
-  render() {
-    const navbar = <NavBar />;
+      this.state = {
+        authenticated: null,
+        user: null,
+        api: new Api()
+      };
 
-    return (
-      <Router>
+      this.checkAuthentication = this.checkAuthentication.bind(this);
+    }
+
+    async checkAuthentication() {
+      const authenticated = await this.props.auth.isAuthenticated();
+
+      if (authenticated !== this.state.authenticated) {
+        if (authenticated) {
+          const user = await this.props.auth.getUser();
+          let accessToken = await this.props.auth.getAccessToken();
+          this.setState({ authenticated, user, api: new Api(accessToken) });
+        } else {
+          this.setState({ authenticated, user: null, api: new Api() });
+        }
+      }
+    }
+
+    async componentDidMount() {
+      this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+      this.checkAuthentication();
+    }
+
+    async login() {
+      if (this.state.authenticated === null) return;
+
+      this.props.auth.login("/");
+    }
+
+    async logout() {
+      this.props.auth.logout("/");
+    }
+
+    render() {
+      let { authenticated, user, api } = this.state;
+
+      if (authenticated === null) {
+        return null;
+      }
+
+      const navbar = (
+        <NavBar
+          isAuthenticated={authenticated}
+          login={this.login.bind(this)}
+          logout={this.logout.bind(this)}
+        />
+      );
+
+      return (
         <Switch>
           <Route
             path="/"
             exact={true}
-            render={props => <Home {...props} api={api} navbar={navbar} />}
+            render={props => (
+              <Home
+                {...props}
+                authenticated={authenticated}
+                user={user}
+                api={api}
+                navbar={navbar}
+              />
+            )}
           />
-          <Route
+          <SecureRoute
             path="/coffee-shops"
             exact={true}
             render={props => (
-              <CoffeeShopsList {...props} api={api} navbar={navbar} />
+              <CoffeeShopsList
+                {...props}
+                authenticated={authenticated}
+                user={user}
+                api={api}
+                navbar={navbar}
+              />
             )}
           />
-          <Route
+          <SecureRoute
             path="/coffee-shops/:id"
             render={props => (
-              <CoffeeShopEdit {...props} api={api} navbar={navbar} />
+              <CoffeeShopEdit
+                {...props}
+                authenticated={authenticated}
+                user={user}
+                api={api}
+                navbar={navbar}
+              />
             )}
           />
         </Switch>
+      );
+    }
+  }
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+          issuer="https://dev-711549.okta.com/oauth2/default"
+          clientId="0oayh9qxYCMUDZA4x4x5"
+          redirectUri={window.location.origin + "/implicit/callback"}
+          pkce={true}
+        >
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+          <AuthWrapper />
+        </Security>
       </Router>
     );
   }
